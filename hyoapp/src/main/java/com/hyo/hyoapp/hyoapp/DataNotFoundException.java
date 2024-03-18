5cf53ddcb2448035a302f92dbf2aa3ebf35c899a
seongsu <aidzero01@gmail.com>
2024-03-18 22:08:41 +0900

    h2 database and creat bullet board

--- hyoapp/src/main/java/com/hyo/hyoapp/hyoapp/DataNotFoundException.java
+++ hyoapp/src/main/java/com/hyo/hyoapp/hyoapp/DataNotFoundException.java
@@ -8,5 +8,6 @@ public class DataNotFoundException extends RuntimeException{
     private static final long serialVersionUID = 1L;
     public DataNotFoundException(String message){
         super(message);
+        
     }
 }
