7383b53a132da0c555f32388bdc27f942d7a1d87
seongsu <aidzero01@gmail.com>
2024-03-19 13:41:56 +0900

    테스트케이스로 질문글 반복생성

--- hyoapp/src/test/java/com/hyo/hyoapp/HyoappApplicationTests.java
+++ hyoapp/src/test/java/com/hyo/hyoapp/HyoappApplicationTests.java
@@ -1,33 +1,24 @@
 package com.hyo.hyoapp;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Optional;
-import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.hyo.hyoapp.hyoapp.answer.Answer;
-import com.hyo.hyoapp.hyoapp.answer.AnswerRepository;
-import com.hyo.hyoapp.hyoapp.question.QuestionRepository;
+import com.hyo.hyoapp.hyoapp.HyoappApplication;
+import com.hyo.hyoapp.hyoapp.question.QuestionService;
 
-@SpringBootTest
-class HyoappApplicationTests {
+@SpringBootTest(classes = HyoappApplication.class)
+class HyoappApplicationTests{
 
-	@Autowired
-	private QuestionRepository questionRepository;
-
-	@Autowired
-    private AnswerRepository answerRepository;
+    @Autowired
+    private QuestionService questionService;
 
     @Test
-    void testJpa() {
-        Optional<Answer> oa = this.answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(2, a.getQuestion().getId());
-	}
-
-
+    void testJpa(){
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
+    }
 }
